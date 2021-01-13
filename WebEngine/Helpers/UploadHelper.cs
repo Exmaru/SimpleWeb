d7b292ca536cf08dc26413818159aae40cb00a55
Parkheesung <yunlang97@gmail.com>
2021-01-13 16:50:51 +0900

    파일위치 변경

---
+++ WebEngine/Helpers/UploadHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebEngine
+{
+    public class UploadHelper : IDisposable
+    {
+        public UploadHelper()
+        {
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
